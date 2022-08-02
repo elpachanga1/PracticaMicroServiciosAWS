575d89cef1fdc52da4bc189ef933128cd1956879
elpachanga1 <fabianguerrero9328@gmail.com>
2022-08-01 21:15:17 -0500

    gitignore

---
+++ Microservicios/appclient/src/main/java/com/pachanga/appclient/AppclientApplication.java
@@ -0,0 +1,13 @@
+package com.pachanga.appclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppclientApplication.class, args);
+	}
+
+}
